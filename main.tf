1b238acdfd28697e61890efd1ab05f4735e0ee40
Odon Nkongolo <odonnkongolo@gmail.com>
2026-06-02 19:43:56 +0100

    feat: add observability stack (Prometheus, Grafana, cAdvisor) via Terraform

--- main.tf
+++ main.tf
@@ -90,3 +90,99 @@ resource "docker_container" "nginx" {
   # Ensure the web container starts first
   depends_on = [docker_container.web]
 }
+
+# --- Observability Stack ---
+
+resource "docker_image" "prometheus_image" {
+  name = "prom/prometheus:latest"
+}
+
+resource "docker_image" "grafana_image" {
+  name = "grafana/grafana:latest"
+}
+
+resource "docker_image" "cadvisor_image" {
+  name = "gcr.io/cadvisor/cadvisor:v0.47.0"
+}
+
+# Prometheus Container
+resource "docker_container" "prometheus" {
+  name  = "prometheus"
+  image = docker_image.prometheus_image.name
+  restart = "unless-stopped"
+
+  networks_advanced {
+    name = docker_network.cctv_net.name
+    aliases = ["prometheus"]
+  }
+
+  ports {
+    internal = 9090
+    external = 9090
+    protocol = "tcp"
+  }
+
+  volumes {
+    host_path      = "/Users/ojayodon/Developer/cctv-simulator/monitoring/prometheus.yml"
+    container_path = "/etc/prometheus/prometheus.yml"
+    read_only      = true
+  }
+}
+
+# Grafana Container
+resource "docker_container" "grafana" {
+  name  = "grafana"
+  image = docker_image.grafana_image.name
+  restart = "unless-stopped"
+
+  networks_advanced {
+    name = docker_network.cctv_net.name
+    aliases = ["grafana"]
+  }
+
+  ports {
+    internal = 3000
+    external = 3000
+    protocol = "tcp"
+  }
+}
+
+# cAdvisor Container
+resource "docker_container" "cadvisor" {
+  name  = "cadvisor"
+  image = docker_image.cadvisor_image.name
+  restart = "unless-stopped"
+  privileged = true
+
+  networks_advanced {
+    name = docker_network.cctv_net.name
+    aliases = ["cadvisor"]
+  }
+
+  ports {
+    internal = 8080
+    external = 8080
+    protocol = "tcp"
+  }
+
+  volumes {
+    host_path      = "/"
+    container_path = "/rootfs"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/run"
+    container_path = "/var/run"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/sys"
+    container_path = "/sys"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/lib/docker"
+    container_path = "/var/lib/docker"
+    read_only      = true
+  }
+}
